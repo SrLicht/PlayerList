1429dadf2eeb720ffba9e0eb6d83b42c2d65ad58
Franco <francoquiroz_13@live.com>
2021-08-23 07:12:10 -0300

    First Commit

---
+++ PlayerList/PlayerList/Command.cs
@@ -0,0 +1,149 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Extensions;
+using RemoteAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerList
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Command : ICommand
+    {
+        public string[] Aliases => PlayerList.Instance.Config.CommandAlias;
+
+        public string Description => PlayerList.Instance.Config.CommandDescription;
+
+        string ICommand.Command => PlayerList.Instance.Config.CommandName;
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (sender != null)
+            {
+                // RemoteAdmin
+                if (sender is PlayerCommandSender player)
+                {
+                    if (Player.List.Count() >= 1)
+                    {
+                        int max = GameCore.ConfigFile.ServerConfig.GetInt("max_players", 20);
+                        int cur = Player.List.Count();
+                        TimeSpan duration = Round.ElapsedTime;
+                        string seconds = duration.Seconds < 10 ? $"0{duration.Seconds}" : duration.Seconds.ToString();
+                        string minutes = duration.Minutes < 10 ? $"0{duration.Minutes}" : duration.Minutes.ToString();
+                        string msg;
+                        msg = string.Format(PlayerList.Instance.Config.TextTimeFormat, minutes, seconds);
+                        msg += string.Format(PlayerList.Instance.Config.TextTotalPlayersFormat, cur, max);
+                        msg += PlayerList.Instance.Config.TextTitle;
+                        foreach (Player ply in Player.List)
+                        {
+                            Log.Info("En el foreach");
+                            if (ply.RemoteAdminAccess && PlayerList.Instance.Config.RemarksStaff)
+                            {
+                                msg += string.Format(PlayerList.Instance.Config.TextperStaffPlayer, ply.Id, ply.Nickname, ply.Role);
+                            }
+                            else
+                            {
+                                msg += string.Format(PlayerList.Instance.Config.TextperPlayer, ply.Id, ply.Nickname, ply.Role);
+                            }
+                        }
+                        response = msg;
+                        return true;
+
+                    }
+                    else
+                    {
+
+                        string msg = PlayerList.Instance.Config.NoPlayersOnline;
+                        response = msg;
+                        return true;
+
+                    }
+                }
+                else
+                {
+                    // Bot or Console
+                    if (Player.List.Count() >= 1)
+                    {
+                        int max = GameCore.ConfigFile.ServerConfig.GetInt("max_players", 20);
+                        int cur = Player.List.Count();
+                        TimeSpan duration = Round.ElapsedTime;
+                        string seconds = duration.Seconds < 10 ? $"0{duration.Seconds}" : duration.Seconds.ToString();
+                        string minutes = duration.Minutes < 10 ? $"0{duration.Minutes}" : duration.Minutes.ToString();
+                        string msg;
+
+                        if (PlayerList.Instance.Config.UseDiscordCodeBlock)
+                        {
+                            msg = $"```{PlayerList.Instance.Config.WhatTypeOfCodeBlock}\n";
+                            msg += string.Format(PlayerList.Instance.Config.TextTimeFormat, minutes, seconds);
+                            msg += string.Format(PlayerList.Instance.Config.TextTotalPlayersFormat, cur, max);
+                            msg += $"{PlayerList.Instance.Config.TextTitle}";
+
+                            foreach (Player ply in Player.List)
+                            {
+                                if (ply.RemoteAdminAccess && PlayerList.Instance.Config.RemarksStaff)
+                                {
+                                    msg += string.Format(PlayerList.Instance.Config.TextperStaffPlayer, ply.Id, ply.Nickname, ply.Role);
+                                }
+                                else
+                                {
+                                    msg += string.Format(PlayerList.Instance.Config.TextperPlayer, ply.Id, ply.Nickname, ply.Role);
+                                }
+                            }
+                            msg += "\n```";
+                            response = msg;
+                            return true;
+                        }
+                        else
+                        {
+
+
+                            msg = string.Format(PlayerList.Instance.Config.TextTimeFormat, minutes, seconds);
+                            msg += string.Format(PlayerList.Instance.Config.TextTotalPlayersFormat, cur, max);
+                            msg += $"{PlayerList.Instance.Config.TextTitle}";
+
+                            foreach (Player ply in Player.List)
+                            {
+                                if (ply.RemoteAdminAccess && PlayerList.Instance.Config.RemarksStaff)
+                                {
+                                    msg += string.Format(PlayerList.Instance.Config.TextperStaffPlayer, ply.Id, ply.Nickname, ply.Role);
+                                }
+                                else
+                                {
+                                    msg += string.Format(PlayerList.Instance.Config.TextperPlayer, ply.Id, ply.Nickname, ply.Role);
+                                }
+                            }
+                            response = msg;
+                            return true;
+                        }
+
+                    }
+                    else
+                    {
+                        if (PlayerList.Instance.Config.UseDiscordCodeBlock)
+                        {
+                            string msg = $"```{PlayerList.Instance.Config.WhatTypeOfCodeBlock}\n";
+                            msg += PlayerList.Instance.Config.NoPlayersOnline;
+                            msg += "\n```";
+                            response = msg;
+                            return true;
+                        }
+                        else
+                        {
+                            string msg = PlayerList.Instance.Config.NoPlayersOnline;
+                            response = msg;
+                            return true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                response = "Sender is null";
+                return false;
+            }
+        }
+    }
+}
---
+++ PlayerList/PlayerList/PlayerList.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerList
+{
+    public class PlayerList : Plugin<Config>
+    {
+        private static readonly PlayerList Singleton = new PlayerList();
+
+        private PlayerList()
+        {
+        }
+
+        /// <summary>
+        /// Gets the only existing instance of this plugin.
+        /// </summary>
+        public static PlayerList Instance => Singleton;
+
+        public override string Author => "SrLicht";
+        public override Version Version => new Version(1, 0, 6);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+
+    }
+}
